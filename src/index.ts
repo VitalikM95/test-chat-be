d742b8c3241a9cfdd840fd1ee7ebe157c568f91b
Vitalik <marychvitalik95@gmail.com>
2024-08-27 22:40:23 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,6 @@ dotenv.config()
 
 const app = express()
 const server = createServer(app)
-const PORT = process.env.PORT || 5000
 
 connectDB()
 
@@ -29,6 +28,7 @@ app.use((err: AppError, req: Request, res: Response, next: NextFunction) => {
   })
 })
 
+const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
